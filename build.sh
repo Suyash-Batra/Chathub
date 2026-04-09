e7fad2471255312e82006e9d629748c08c42ebad
Suyash Batra <suyash8075b@gmail.com>
2026-04-09 17:23:26 +0530

    Simplify build script by removing redundant commands
    Removed unnecessary pip upgrade and migration commands.

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 
-# 1. Force a "fake" reset of the migration state 
-# This cleans up the mess from previous failed attempts
-python manage.py migrate --fake base 0001 || true
-
-# 2. Run the actual migration to create django_session and others
+# This MUST run. If the build crashes before this, the 1146 error stays.
 python manage.py migrate --fake-initial
