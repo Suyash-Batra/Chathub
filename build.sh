989e5d8ee337329e9a991954115069291bde007d
Suyash Batra <suyash8075b@gmail.com>
2026-04-09 16:13:51 +0530

    Remove unnecessary commands from build script

--- build.sh
+++ build.sh
@@ -1,18 +1,2 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# 1. Install dependencies
-pip install --upgrade pip setuptools wheel
-pip install --only-binary :all: --prefer-binary -r requirements.txt
-
-# 2. Collect Static Files
 python manage.py collectstatic --no-input
-
-# 3. FIX THE GHOST MIGRATIONS
-# This tells Django to "forget" it faked the core apps so it can run them for real
-python manage.py migrate sessions zero || true
-python manage.py migrate auth zero || true
-
-# 4. Now run them for real (No --fake flag!)
 python manage.py migrate
