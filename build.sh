0431eec001741813b2fe6c72af4b72d0a9a55d3f
Suyash Batra <suyash8075b@gmail.com>
2026-04-09 18:56:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# This will now recreate django_content_type, auth_permission, 
-# and django_session from scratch. 
-# --fake-initial will skip your 'base_message' and 'base_room' 
-# so you don't lose your actual chat data.
-python manage.py migrate --fake-initial
+python manage.py migrate
