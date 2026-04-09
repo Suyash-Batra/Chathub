3a54179cb3b17563441d3c769a2a20e622190ccf
Suyash Batra <suyash8075b@gmail.com>
2026-04-09 16:16:48 +0530

    Enhance build script with error handling and dependencies
    Added error handling and improved dependency installation.

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# 1. Install dependencies (Crucial step!)
+pip install --upgrade pip setuptools wheel
+pip install --only-binary :all: --prefer-binary -r requirements.txt
+
+# 2. Collect Static Files
 python manage.py collectstatic --no-input
+
+# 3. Apply Database Migrations
 python manage.py migrate
