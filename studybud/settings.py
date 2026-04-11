1cc0d7411868006f35cb71a689e2534b13f8645c
Suyash Batra <suyash8075b@gmail.com>
2026-04-12 00:11:33 +0530

    Reorder INSTALLED_APPS for Cloudinary compatibility

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,16 +26,16 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
-    'cloudinary_storage',
+    # CLOUDINARY MUST BE ABOVE STATICFILES
+    'cloudinary_storage', 
+    'django.contrib.staticfiles',
     'cloudinary',
 
     # Third party apps
