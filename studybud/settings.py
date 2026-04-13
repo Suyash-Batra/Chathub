e309cd1f10f53aa103a895677fad76874a2e5396
Suyash-Batra <Suyash8075b@gmail.com>
2026-04-13 15:09:24 +0530

    fixed settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -130,8 +130,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- SECURITY SETTINGS ---
 CSRF_TRUSTED_ORIGINS = ["https://chathub-72tx.onrender.com"]
